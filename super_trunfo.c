a86982a03c3d7136a836b0cb870587f5b56be9fc
Guilhermeprogramador070404 <guiprogramador07@gmail.com>
2025-02-24 17:18:40 +0000

    criei carta 1

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 
 int main(){
-    char estado;
+    printf("Cartas Super Trunfo!\n");
+    printf(" Carta 1\n");
+    
+    char estado[1];
     char codigo[3];
     char nome[20];
-    int população;
-    float área;
-    float PIB;
-    int NPT;
-
+    int populacao;
+    float area;
+    float pib;
+    int npt;
+    
     printf("Nome do Estado: \n");
     scanf("%s", &estado);
+    printf("Nome do Estado é: %s\n", estado);
 
     printf("Código da Carta: \n");
     scanf("%s", &codigo);
+    printf("Código da Carta é: %s\n", codigo);
 
     printf("Nome da Cidade: \n");
     scanf("%s", &nome);
+    printf("Nome da Cidade é: %s\n", nome);
 
     printf("População: \n");
-    scanf("%d", &população);
+    scanf("%d", &populacao);
+    printf("População é: %d\n", populacao);
 
     printf("Área: \n");
-    scanf("%f", &área);
+    scanf("%f", &area);
+    printf("Área(Km²) é: %f\n", area);
 
     printf("PIB: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &pib);
+    printf("PIB é: %f\n", pib);
 
     printf("NPT: \n");
-    scanf("%d", &NPT);
+    scanf("%d", &npt);
+    printf("Número de Pontos Turísticos é: %d\n", npt);
+
     
-
-
-
-
-
-
-
-
-
 }
