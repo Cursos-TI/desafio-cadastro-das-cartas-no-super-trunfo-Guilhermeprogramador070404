5bedd8ee69f43b5acb3a8c1d59ca23aff01cb404
Guilhermeprogramador070404 <guiprogramador07@gmail.com>
2025-02-24 16:32:17 +0000

    criei a carta 1

---
+++ super_trunfo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+    char estado;
+    char codigo[3];
+    char nome[20];
+    int população;
+    float área;
+    float PIB;
+    int NPT;
+
+    printf("Nome do Estado: \n");
+    scanf("%s", &estado);
+
+    printf("Código da Carta: \n");
+    scanf("%s", &codigo);
+
+    printf("Nome da Cidade: \n");
+    scanf("%s", &nome);
+
+    printf("População: \n");
+    scanf("%d", &população);
+
+    printf("Área: \n");
+    scanf("%f", &área);
+
+    printf("PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("NPT: \n");
+    scanf("%d", &NPT);
+    
+
+
+
+
+
+
+
+
+
+}
