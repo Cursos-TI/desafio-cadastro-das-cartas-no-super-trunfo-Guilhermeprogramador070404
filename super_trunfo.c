1fdd7478a120ab3048575ef464d5197c981a0b03
Guilhermeprogramador070404 <guiprogramador07@gmail.com>
2025-02-24 17:45:52 +0000

    criei duas cartas

--- super_trunfo.c
+++ super_trunfo.c
@@ -40,5 +40,36 @@ int main(){
     scanf("%d", &npt);
     printf("Número de Pontos Turísticos é: %d\n", npt);
 
+    printf("Carta 2\n");
+
+    printf("Nome do Estado: \n");
+    scanf("%s", &estado);
+    printf("Nome do Estado é: %s\n", estado);
+
+    printf("Codigo da Carta: \n");
+    scanf("%s", &codigo);
+    printf("Codigo da Cartra é: %s\n", codigo);
+
+    printf("Nome da Cidade: \n");
+    scanf("%s", &nome);
+    printf("Nome da Cidade é: %s\n", nome);
+
+    printf("Polupação: \n");
+    scanf("%d", &populacao);
+    printf("População é: %d\n", populacao);
+
+    printf("Área: \n");
+    scanf("%f", &area);
+    printf("Área(Km²) é: %f\n", area);
+
+    printf("PIB: \n");
+    scanf("%f", &pib);
+    printf("PIB é: %f\n", pib);
+
+    printf("NPT: \n");
+    scanf("%d", &npt);
+    printf("Número de Pontos Turísticos é: %d\n", npt);
+
+
     
 }
